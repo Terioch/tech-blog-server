42d237b0f57864038c8fdee9242629e5100ab584
Rio <riostockton@gmail.com>
2021-12-07 17:02:43 +0000

    Configured post comments controller and database model

---
+++ Controllers/PostCommentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace TechBlog.Controllers
+{
+    [Authorize(Roles = "Admin, User")]
+    public class PostCommentController : ControllerBase
+    {
+        public ActionResult<int> Get()
+        {
+            return 0;
+        }
+    }
+}
--- Models/PostComment.cs
+++ Models/PostComment.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 namespace TechBlog.Models
 {
     public class PostComment
-    {
+    {        
         public int Id { get; set; }
-        public string Value { get; set; }
         public int PostId { get; set; }
+        public string Value { get; set; }      
         public string SenderUsername { get; set; }
-        public long Date { get; set; }
+        public DateTime Date { get; set; }
     }
 }
