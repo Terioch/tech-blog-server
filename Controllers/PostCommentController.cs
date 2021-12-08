a7039d360ac3b4b66ad2abac00c8405ff053c97f
Rio <riostockton@gmail.com>
2021-12-08 09:59:56 +0000

    configured postComments controller api routes

--- Controllers/PostCommentController.cs
+++ Controllers/PostCommentController.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Web.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TechBlog.Models;
+using TechBlog.Services;
 
 namespace TechBlog.Controllers
 {
+    [ApiController]
+    [Route("/api/{postId}/comments")]
     [Authorize(Roles = "Admin, User")]
     public class PostCommentController : ControllerBase
     {
-        public ActionResult<int> Get()
+        private ICommentDataService repository;
+
+        public PostCommentController(ICommentDataService repository)
         {
-            return 0;
+            this.repository = repository;
+        } 
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PostCommentModel>> Get()
+        {
+            List<PostCommentModel> comments = repository.GetAllComments();
+            return comments;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PostCommentModel> GetOne(int id)
+        {
+            List<PostCommentModel> comments = repository.GetCommentById(id);
+            return comments[0];
         }
     }
 }
