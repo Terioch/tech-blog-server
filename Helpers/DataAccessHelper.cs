480114d5adc1f3a66622572afe52d16590b18176
Rio <riostockton@gmail.com>
2022-02-24 15:19:27 +0000

    Added readme

--- Helpers/DataAccessHelper.cs
+++ Helpers/DataAccessHelper.cs
@@ -16,8 +16,7 @@ namespace TechBlog.Utility
         {
             string databaseUrl = config["DATABASE_URL"];
             Uri uri = new Uri(databaseUrl);
-            return $"host={uri.Host};username={uri.UserInfo.Split(':')[0]};password={uri.UserInfo.Split(':')[1]};database={uri.LocalPath.Substring(1)};sslmode=Prefer;Trust Server Certificate=true";            
-            // return "Host=ec2-34-233-157-9.compute-1.amazonaws.com;Port=5432;Database=d7dh8fvadbcgvl;Username=lujeyzvxqmtgbu;Password=e873972673ea76fba3a3f81ba7d6075dec51f43d80d7f0e406b9dd025f426be6;Pooling=true;sslmode=require;Trust Server Certificate=true;";
+            return $"host={uri.Host};username={uri.UserInfo.Split(':')[0]};password={uri.UserInfo.Split(':')[1]};database={uri.LocalPath.Substring(1)};sslmode=Prefer;Trust Server Certificate=true";                        
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Tech Blog Server
+
+An Asp.Net Web API service for a Technology Blog website using RESTful architecture.
+
+## General Information
+
+This project served as my introduction to Asp.Net Web API and handles requests for the following:
+
+* Blog posts
+* Post comments
+* User login
+* User registration
+
+This service also implements authentication with JWT, claims-based authorization and data access using entity framework.
